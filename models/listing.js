478fed1d6eb874a6776261846e711c43406eb10c
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-02-11 14:14:53 +0530

    added login after signup in user.js

---
+++ models/listing.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Review = require("./review.js");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    } ,
+    description: String,
+    image: {
+        type: String,
+        filename: String,
+        default: "https://plus.unsplash.com/premium_photo-1687960117069-567a456fe5f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (url) => url === "" ? "https://plus.unsplash.com/premium_photo-1687960117069-567a456fe5f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : url,
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing){
+        await Review.deleteMany({_id: {$in: listing.reviews}});
+    }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
