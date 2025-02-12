2518d61d77024844ad7b911f7773e016714ebe03
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-02-12 14:33:11 +0530

    added owner id index file

--- init/index.js
+++ init/index.js
@@ -17,6 +17,8 @@ async function main(){
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "67aa3bb040e3e0ee4acf30ec"
+    }));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized");
 };
