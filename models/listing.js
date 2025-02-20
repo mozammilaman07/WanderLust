7c65e2bc7c06ae2476c5791f8d15ef647584da80
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-02-20 23:04:24 +0530

    code completed

--- models/listing.js
+++ models/listing.js
@@ -9,10 +9,9 @@ const listingSchema = new Schema({
     } ,
     description: String,
     image: {
-        type: String,
+        url: String,
         filename: String,
-        default: "https://plus.unsplash.com/premium_photo-1687960117069-567a456fe5f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (url) => url === "" ? "https://plus.unsplash.com/premium_photo-1687960117069-567a456fe5f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : url,
+     
     },
     price: Number,
     location: String,
